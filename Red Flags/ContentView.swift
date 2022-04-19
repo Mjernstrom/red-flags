5f6a4bf5240bddb92db7c3ceedff0ccf104d81e9
Matthew Jernstrom <mjerns1@lsu.edu>
2022-04-19 09:13:30 -0500

    Initial Commit

---
+++ Red Flags/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Red Flags
+//
+//  Created by Matthew Jernstrom on 10/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Red Flags")
+            .padding()
+            .background(Color.red)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Red Flags/Red_FlagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Red_FlagsApp.swift
+//  Red Flags
+//
+//  Created by Matthew Jernstrom on 10/24/21.
+//
+
+import SwiftUI
+
+@main
+struct Red_FlagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
